a3e59e3259582731a6f46a5e84090f521da257f7
Priyanshu150 <priyanshukumarsingh1502@gmail.com>
2025-01-17 17:00:16 +0530

    Updated code of To Do List

--- To_Do_List/src/main.jsx
+++ To_Do_List/src/main.jsx
@@ -8,6 +8,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-//2. When the add button is pressed, the current data in the input should be
-//added to an array.
-//3. The <ul> should display all the array items as <li>s
